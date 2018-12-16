00d58a9dd7fae38bc9a29fa75e48e24f4844d92c
Rino Reji Cheriyan <Rino.RejiCheriyan@xe04.ey.com>
2018-12-16 15:12:07 +0530

    Updates

---
+++ precache-manifest.bc3a566b34e97baaa36466e4b256cea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7d7e8cf9e37c938c4bf",
+    "url": "/rays-app/static/js/runtime~main.a7d7e8cf.js"
+  },
+  {
+    "revision": "96406e60deb0586ff896",
+    "url": "/rays-app/static/js/main.96406e60.chunk.js"
+  },
+  {
+    "revision": "af16931dc63661748c7c",
+    "url": "/rays-app/static/js/1.af16931d.chunk.js"
+  },
+  {
+    "revision": "96406e60deb0586ff896",
+    "url": "/rays-app/static/css/main.544523f8.chunk.css"
+  },
+  {
+    "revision": "2170b3ab54671ff436a62239aebe3f45",
+    "url": "/rays-app/index.html"
+  }
+];
